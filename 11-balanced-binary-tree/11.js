c21523c8cd738378873b1a42ad1a5b780d52e9cd
= <leonardo@lanytecnologia.com.br>
2023-01-30 14:58:27 +0000

    finish 11

--- 11-balanced-binary-tree/11.js
+++ 11-balanced-binary-tree/11.js
@@ -1,33 +1,55 @@
-class TreeNode {
-    constructor(val, left, right) {
-        this.val = val === undefined ? 0 : val;
-        this.left = left === undefined ? null : left;
-        this.right = right === undefined ? null : right;
-    }
-}
+// https://leetcode.com/problems/balanced-binary-tree/
+import { TreeNode } from './common/three-node.js';
+
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
 var isBalanced = function (root) {
-    let count = {
-        left: 0,
-        right: 0,
-    };
-    function search(root, direction) {
-        if (root && root[direction]) {
-            count[direction]++;
-            search(root[direction], direction);
+    let max = 0;
+    function search(root, depth = 0) {
+        let leftDepth = depth,
+            rightDepth = depth;
+
+        if (root) {
+            if (!root.left && !root.right) {
+                return depth;
+            }
+
+            // if (root.left) {
+            leftDepth = search(root.left, depth + 1);
+            // }
+
+            // if (root.right) {
+            rightDepth = search(root.right, depth + 1);
+            // }
+
+            max = Math.max(Math.abs(leftDepth - rightDepth), max);
+            return Math.max(leftDepth, rightDepth);
         }
+        return 0;
     }
-    search(root, 'right');
-    search(root, 'left');
-    return Math.abs(count.right - count.left) < 2;
+    search(root);
+    return max < 2;
 };
+
+//                1
+//        2                  3
+//    4      5            6     null
+// 8
+// console.log(
+//     isBalanced(
+//         new TreeNode(
+//             1,
+//             new TreeNode(2, new TreeNode(4, new TreeNode(8)), new TreeNode(5)),
+//             new TreeNode(3, new TreeNode(6))
+//         )
+//     )
+// ); // false
 
 // console.log(
 //     isBalanced(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))))
-// );
+// ); // true
 // console.log(
 //     isBalanced(
 //         new TreeNode(
@@ -36,6 +58,18 @@ var isBalanced = function (root) {
 //             new TreeNode(2)
 //         )
 //     )
-// );
-// [1,2,3,4,5,6,null,8]
-// console.log()
+// ); // false
+
+//                1
+//        2                  3
+//    4      5            6     null
+//8
+
+// 1
+//    2
+//      3
+
+//                           1
+//            2                             2
+//      3            null            null                3
+// 4        null                                null            4
---
+++ 11-balanced-binary-tree/v2-11.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+
+// The same as v1 but with a flag to stop searching when found that its instable
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+    let max = 0;
+    let unstable = false;
+    function search(root, depth = 0) {
+        if (unstable) {
+            return -1;
+        }
+
+        let leftDepth = depth,
+            rightDepth = depth;
+        if (root) {
+            if (!root.left && !root.right) {
+                return depth;
+            }
+
+            if (root.left) {
+                leftDepth = search(root.left, depth + 1);
+            }
+
+            if (root.right) {
+                rightDepth = search(root.right, depth + 1);
+            }
+
+            max = Math.max(Math.abs(leftDepth - rightDepth), max);
+            unstable = max > 1;
+            return Math.max(leftDepth, rightDepth);
+        }
+        return 0;
+    }
+    search(root);
+    return !unstable;
+};
