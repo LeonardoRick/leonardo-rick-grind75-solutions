628d948a758d51f4cb104135bd905af0e7aebec4
Leonardo Rick <leonardo@lanytecnologia.com.br>
2024-10-22 14:58:28 +0100

    fix small missing space

--- others/grokking-algorithms/cap6/cap6-01-breadth-first-search.js
+++ others/grokking-algorithms/cap6/cap6-01-breadth-first-search.js
@@ -2,7 +2,7 @@
 // onenote:https://d.docs.live.net/9c46bfb4bfc3c49a/Documentos/Cadernos%20One%20Note/Develop/Books.one#Grokking%20algorithms&section-id={3E71B070-47FD-A548-A5F1-50BE6A6838F9}&page-id={EEF29FC7-6623-804F-BE56-BA1473A0F794}&object-id={78CF0378-B448-FA4E-82A2-CF703324F8E2}&10
 
 const graph = {}
-graph.you= ['alice', 'bob', 'claire'];
+graph.you = ['alice', 'bob', 'claire'];
 graph.bob = ['anuj', 'peggy'];
 graph.alice = ['peggy'];
 graph.claire = ['thom', 'jonny'];
