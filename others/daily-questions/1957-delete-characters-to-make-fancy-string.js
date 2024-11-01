972f8487fe1dd5c6424fed325548c4fcefd11317
Leonardo Rick <leonardo@lanytecnologia.com.br>
2024-11-01 18:28:39 +0000

    cerate 1957-delete-characters-to-make-fancy-strings.js

---
+++ others/daily-questions/1957-delete-characters-to-make-fancy-string.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=1957 lang=javascript
+ *
+ * [1957] Delete Characters to Make Fancy String
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+    let fancy = '';
+    let count = 0;
+    let lastCharacter = '';
+
+    for (let character of s) {
+        if (character === lastCharacter) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count < 2) {
+            fancy += character;
+        }
+
+        lastCharacter = character;
+    }
+    return fancy;
+};
+// @lc code=end
+
+console.log(makeFancyString('leeetcode')); // leetcode
