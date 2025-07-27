e69727581b23d9022e4eb58b6f93f643aa4d52bb
Leonardo Rick <leonardo.rick@qonto.com>
2025-07-27 21:04:30 +0100

    fix description of 796 rotate string

--- others/daily-questions/796-rotate-string.js
+++ others/daily-questions/796-rotate-string.js
@@ -2,6 +2,7 @@
  * @lc app=leetcode id=796 lang=javascript
  *
  * [796] Rotate String
+ * https://leetcode.com/problems/rotate-string/
  */
 
 /**
@@ -27,7 +28,7 @@ var rotateString = function(s, goal) {
 // The above solution is valid and works but there's a faster solution:
 // 1. Checking if both strings have the same length
 // 2. Duplicates the s string and check if this duplicated string contains the goal inside of it.
-// If both of this values are true, then shifting the string would eventually endup bringing the goal string to s
+// If both above chcks are true, then shifting the string would eventually endup bringing the goal string to s
 
 // @lc code=start
 /**
